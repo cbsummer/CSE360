fd5f75bd72b648b3651bfef7c5c228ad13d3a336
chase <chase@DESKTOP-97K2RNU>
2019-02-25 18:41:31 -0700

    All methods but getHistory implemented.

--- CSE360Assignments/src/cse360assign2/Calculator.java
+++ CSE360Assignments/src/cse360assign2/Calculator.java
@@ -18,24 +18,49 @@ public class Calculator {
 		total = 0;  // not needed - included for clarity
 	}
 	
+	//Returns total
+	
 	public int getTotal () {
-		return 0;
+		
+		return total;
+	
 	}
+	
+	//Adds to running total
 	
 	public void add (int value) {
-		
+	
+		total += value;
+	
 	}
 	
+	//Subtracts from running total
+	
 	public void subtract (int value) {
-		
+	
+		total -= value;
+	
 	}
+	
+	//Multiplies to running total
 	
 	public void multiply (int value) {
 		
+		total *= value;
+	
 	}
+	
+	//Divides from running total. If value equals zero change total to zero
 	
 	public void divide (int value) {
 		
+		if(value == 0) {
+			total = 0;
+		}
+		
+		else {
+			total /= value;
+		}
 	}
 	
 	public String getHistory () {
